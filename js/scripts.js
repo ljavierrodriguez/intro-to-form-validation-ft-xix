fc7a3ddd2d7cb21667179a39183e01d9021118b4
Luis J. Rodriguez O <ljavierrodriguez@gmail.com>
2023-02-20 17:29:55 +0000

    Testing Events

--- js/scripts.js
+++ js/scripts.js
@@ -15,42 +15,72 @@ home.addEventListener('click', (e) => {
 
 let form = document.querySelector('form');
 
-form.addEventListener('submit', (evento) => {
-    evento.preventDefault();
-    let enviar = true;
-    //let email = document.getElementById('email');
-    let username = document.querySelector('#username');
-    let errUsername = document.querySelector('.error-username');
-    
-    let password = document.querySelector('#password');
-    let errPass = document.querySelector('.error-password');
+if (form) {
+    form.addEventListener('submit', (evento) => {
+        evento.preventDefault();
+        let enviar = true;
+        //let email = document.getElementById('email');
+        let username = document.querySelector('#username');
+        let errUsername = document.querySelector('.error-username');
 
-    if(username.value === ''){
-        enviar = false;
-        username.classList.add('is-invalid')
-        errUsername.innerHTML = "El campo no puede estar vacio";
-    } else if (!isEmail.test(username.value)){
-        enviar = false;
-        username.classList.add('is-invalid')
-        errUsername.innerHTML = "El campo debe ser un email";
-    } else {
-        username.classList.remove('is-invalid')
-    }
+        let password = document.querySelector('#password');
+        let errPass = document.querySelector('.error-password');
 
-    if(password.value === ''){
-        enviar = false;
-        password.classList.add('is-invalid')
-        errPass.innerHTML = "El campo no puede estar vacio";
-    } else if(!soloNumeros.test(password.value)){
-        enviar = false;
-        password.classList.add('is-invalid')
-        errPass.innerHTML = "El password no cumple el formato indicado";
-    } else {
-        password.classList.remove('is-invalid')
-    }
+        if (username.value === '') {
+            enviar = false;
+            username.classList.add('is-invalid')
+            errUsername.innerHTML = "El campo no puede estar vacio";
+        } else if (!isEmail.test(username.value)) {
+            enviar = false;
+            username.classList.add('is-invalid')
+            errUsername.innerHTML = "El campo debe ser un email";
+        } else {
+            username.classList.remove('is-invalid')
+        }
 
-    if(enviar){
-        form.submit();
-    }
+        if (password.value === '') {
+            enviar = false;
+            password.classList.add('is-invalid')
+            errPass.innerHTML = "El campo no puede estar vacio";
+        } else if (!soloNumeros.test(password.value)) {
+            enviar = false;
+            password.classList.add('is-invalid')
+            errPass.innerHTML = "El password no cumple el formato indicado";
+        } else {
+            password.classList.remove('is-invalid')
+        }
 
-});
+        if (enviar) {
+            form.submit();
+        }
+
+    });
+}
+
+let contenido = document.querySelector('.contenido');
+
+let btnClick = document.querySelector('.btn-click');
+console.log(btnClick);
+btnClick.addEventListener('click', (evt) => {
+    evt.target.innerHTML = 'Clicked!'
+})
+
+let newButton = document.createElement('button');
+newButton.classList.add('btn', 'btn-primary', 'btn-sm');
+newButton.innerHTML = "Boton desde el DOM"
+
+newButton.addEventListener('dblclick', (evt) => {
+    evt.target.innerHTML = 'Clicked!'
+})
+
+contenido.appendChild(newButton);
+
+let btnInfo = document.querySelectorAll('.btn-info'); // [HTMLButtonElement]
+
+console.log(btnInfo);
+
+btnInfo.forEach((btn) => {
+    btn.addEventListener('click', (evt) => {
+        evt.target.innerHTML = parseInt(evt.target.innerHTML) + 1;
+    })
+})
